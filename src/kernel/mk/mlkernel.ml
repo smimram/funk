7242f644fc15d708a1acd7c366314efc50fe7c4a
Alexandre Buisse <ab@alexandrebuisse.org>
2005-07-22 02:46:53 +0000

    New branch ! (hahahaha, Brice est en vacances)

--- src/kernel/mk/mlkernel.ml
+++ src/kernel/mk/mlkernel.ml
@@ -64,6 +64,8 @@ let mlkernel_entry arg =
     Printf.printf "\n%!";
     Printf.printf "Funk 0.1.0 : caml est dans le jazz\n\n%!";
 
+    (* Satan *)
+    (*
     let context = {
       Funk.uid = 0;
       Funk.gid = 0;
@@ -72,7 +74,7 @@ let mlkernel_entry arg =
       Funk.umask = Vfs_defs.s_IWGRP lor Vfs_defs.s_IWOTH
     }
     in
-      Filecmds.start_dmesg_file_logging context;
+      Filecmds.start_dmesg_file_logging context; *)
       Shell.toplevel context arg
   with
     | e ->
