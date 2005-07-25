c0ee1ee53967ce71c0e8585f2a794c2b12daeab1
Nicolas Guenot <ngue@itu.dk>
2005-07-25 23:49:27 +0000

    No more kernel-clean in funk-mk, funk.ml renamed in utils.ml. Old funk.inb and such should be added later to the kernel

--- src/kernel-clean/boot/i386/include/kernel.h
+++
@@ -1,3 +0,0 @@
-/* linker symbols */
-extern void _begin,_ro_end,_end,__bss_start,__bss_end;
-
--- src/kernel/mk/funk.mli
+++
@@ -1,7 +0,0 @@
-(** Print a kernel message. *)
-val kprintf :  string -> ('a, unit, string, unit) format4 -> 'a
-
-(** Set the definitive routine to write kernel messages in file *)
-val set_fkprintf :  (string -> unit) -> unit
-
-val get_fkprintf : unit -> (string -> unit)
