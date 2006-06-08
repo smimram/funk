47d3d4accf8fa4d4717aa9fc1f6119c8fbfabf26
Samuel Mimram <samuel.mimram@ens-lyon.org>
2006-06-08 20:09:26 +0000

    Added needed fake getrusage function.

--- src/kernel/run/libc-dummy.c
+++ src/kernel/run/libc-dummy.c
@@ -1087,6 +1087,30 @@ int getrlimit64(__rlimit_resource_t __resource,struct rlimit *__rlimits)
   return notImpl_int();
 }
 
+struct rusage {
+  struct timeval ru_utime; /* user time used */
+  struct timeval ru_stime; /* system time used */
+  long   ru_maxrss;        /* maximum resident set size */
+  long   ru_ixrss;         /* integral shared memory size */
+  long   ru_idrss;         /* integral unshared data size */
+  long   ru_isrss;         /* integral unshared stack size */
+  long   ru_minflt;        /* page reclaims */
+  long   ru_majflt;        /* page faults */
+  long   ru_nswap;         /* swaps */
+  long   ru_inblock;       /* block input operations */
+  long   ru_oublock;       /* block output operations */
+  long   ru_msgsnd;        /* messages sent */
+  long   ru_msgrcv;        /* messages received */
+  long   ru_nsignals;      /* signals received */
+  long   ru_nvcsw;         /* voluntary context switches */
+  long   ru_nivcsw;        /* involuntary context switches */
+};
+
+int getrusage(int who, struct rusage* usage)
+{
+  return -1;
+}
+
 static const char _nl_C_LC_CTYPE_class[768] /* attribute_hidden */ =
   /* 0x80 */ "\000\000" "\000\000" "\000\000" "\000\000" "\000\000" "\000\000"
   /* 0x86 */ "\000\000" "\000\000" "\000\000" "\000\000" "\000\000" "\000\000"
